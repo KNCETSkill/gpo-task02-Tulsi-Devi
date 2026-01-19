a4b13fb8f95fc25b1dc267fa4067100e3de1c113
Tulsi Devi M <tulsidevi7213@gmail.com>
2026-01-19 15:40:51 +0530

    Implement attendance status evaluation
    Add attendance evaluation logic based on input percentage.

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,1 +1,14 @@
-
+java.utils*;
+java.util.Scanner*;
+public static void main(String[] args){
+  System.out.println("Enter the attendance: ");
+  Scanner sc = new Scanner(System.in);
+  int n = sc.nextInt();
+  if(n<100 && n>85){
+    System.out.println("Excellent");
+  }else if(n<85 && n>60){
+    System.out.println("Satisfactory");
+  }else(n<60){
+    System.out.println("Poor");
+  }
+  
