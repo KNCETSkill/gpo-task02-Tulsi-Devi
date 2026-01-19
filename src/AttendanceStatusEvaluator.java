0fa12cbfe9db4fffc3574bd44b9478dd67910f05
Tulsi Devi M <tulsidevi7213@gmail.com>
2026-01-19 15:52:01 +0530

    Fix attendance status evaluation conditions

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -6,7 +6,7 @@ public static void main(String[] args){
   int n = sc.nextInt();
   if(n<100 && n>85){
     System.out.println("Excellent");
-  }else if(n<85 && n>60){
+  }else if(n<=85 && n>=60){
     System.out.println("Satisfactory");
   }else(n<60){
     System.out.println("Poor");
